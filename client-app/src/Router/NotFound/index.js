7944c7141155b48c64256f0285f769e5d6bc9573
ChrPass <xristospas97@gmail.com>
2021-02-26 19:16:05 +0200

    Not found improvement

--- client-app/src/Router/NotFound/index.js
+++ client-app/src/Router/NotFound/index.js
@@ -1,7 +1,37 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+  },
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2),
+  },
+}));
+
 const NotFound = () => {
-    return (
-        <div>not found</div>
-    )
-}
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Container component="main" className={classes.main} maxWidth="sm">
+        <Typography variant="h2" component="h1" gutterBottom>
+          Ooops!
+        </Typography>
+        <Typography variant="h5" component="h2" gutterBottom>
+          {
+            "The page that you searched does not exist."
+          }
+        </Typography>
+      </Container>
+    </div>
+  );
+};
 
 export default NotFound;
